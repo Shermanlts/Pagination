fb53597b2222911147a4fe504278fa293a363de1
Robert Sherman <shermanlts@gmail.com>
2018-08-12 14:00:53 -0700

    Library scrape of page working
    Text written to ReadME.
    Content being scrapped from site and stored properly.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Pagination
+
+<h1>Treehouse Techdegree Project 2:</h1>
+<h2>Pagination & Content Filter</h2>
+
+<p>The opject of this project was to take some existing HTML/CSS and add a JavaScript file to the project that would allow for dynamic page changes without affecting the underlying HTML/CSS. This is done so that older browsers without updated JS can still view the content, albeit with a lesser experience.</p>
+<p>This project was developed based on sourcecode by Rob Sherman</p>
+<p>For additional information about Techdegrees or to see their online course list visit https://teamtreehouse.com</p>
--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+JS code by Rob Sherman 8/12/2018
 ******************************************/
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
+// Student constants are used to build the studentLibrary
+const studentSource = document.querySelectorAll('.student-item');
+const studentLinks = document.getElementsByClassName('avatar');
+const studentNames = document.querySelectorAll('.student-details h3');
+const studentEmails = document.getElementsByClassName('email');
+const studentDates = document.getElementsByClassName('date');
+//StudentElement hold the location to be written to.
+const studentElement = document.getElementsByClassName('student-list')[0];
+
+let studentLibrary = [];//holds the current Dictionary of students
+
+//builds the studentLibrary dictionary.
+function buildLibrary(); {
+    for (i = 0; i < studentSource.length; i++) {
+        studentLibrary.push({link: studentLinks[i].getAttribute("src"), name: studentNames[i].textContent, email: studentEmails[i].textContent, 
+                            date: studentDates[i].textContent });
+    }
+}
 
 
+console.log(studentLibrary);
 
+const studentTemplate = 
+`<div class="student-details">
+    <img class="avatar" src=${studentLibrary[0].link}>
+    <h3>${studentLibrary[0].name}</h3>
+    <span class="email">${studentLibrary[0].email}</span>
+</div>
+<div class="joined-details">
+   <span class="date">${studentLibrary[0].date}</span>
+</div>`;
+
+studentElement.innerHTML = studentTemplate;
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
